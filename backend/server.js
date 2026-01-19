2cb8b888d9355c0a6eeee9a97d3d67d975c0f134
gift01-star <chimwazagift7@gmail.com>
2026-01-19 05:16:54 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,7 @@ app.use((req, res, next) => {
     'http://localhost:3001',
     'https://edulove-frontend.onrender.com',
     'https://dating-zujg.onrender.com'
+    'https://frontend-i89x.onrender.com'
   ];
   
   if (allowedOrigins.includes(origin)) {
